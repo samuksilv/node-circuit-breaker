0b8d60207181324b2a7883b6b2ce06b2ddde393e
samuel.carvalho <samuel.carvalho@mag-it.com.br>
2020-02-20 13:35:23 -0300

    implementação com typescript

--- src/index.ts
+++ src/index.ts
@@ -3,70 +3,116 @@ import moment from "moment";
 import CircuitBreaker from "opossum";
 import { ChuckNorrisPhrase } from "./models/chuck-norris-phrase-response";
 
-const baseURL: string = "https://api.chucknorris.io";
+class App {
 
-const api: AxiosInstance = axios.create({
-    baseURL
-});
+    circuit: CircuitBreaker;
 
-const promise = async () => {
-    setInterval(searchChuckNorrisPhrase, 1);
-};
+    constructor() {
 
-async function searchChuckNorrisPhrase(): Promise<void> {
-    const response = await api.get('jokes/random')
+        this.circuit = new CircuitBreaker(this.searchChuckNorrisPhrase, {
+            timeout: 6000,
+            errorThresholdPercentage: 50,
+            resetTimeout: 8000,
+        });
 
-    const data: ChuckNorrisPhrase = response.data as ChuckNorrisPhrase;
-    const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+        this.subscriveEvents();
 
-    console.log(`[${date}] - ${data.value}`);
+    }
+
+
+    public run() {
+
+        setInterval(() => {
+            this.circuit.fire().then(console.log).catch(console.error);
+        }, 5000);
+    }
+
+    searchChuckNorrisPhrase(): Promise<string> {
+
+        const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+        return new Promise<string>(async (resolve, reject) => {
+
+            const baseURL: string = "https://api.chucknorris.io";
+
+            const api = axios.create({
+                baseURL
+            });
+
+            const response = await api.get('jokes/random');
+
+            const data: ChuckNorrisPhrase = response.data as ChuckNorrisPhrase;
+
+            resolve(`RUNNING - [${date}] - ${data.value}`);
+
+        });
+
+    }
+
+    subscriveEvents() {
+
+        this.circuit.fallback(async () => {
+            console.log("teste");
+        });
+
+        this.circuit.on('fallback', (data) => console.log(`unavailable right now. Try later.`, "fallback"));
+
+        this.circuit.on("healthCheckFailed", () => {
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - healthCheckFailed`);
+        });
+
+        this.circuit.on("success", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - circuit success`);
+        });
+
+        this.circuit.on("fire", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - circuit has fired event`);
+        });
+
+        this.circuit.on("failure", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - circuit has failure`);
+        });
+
+        this.circuit.on("close", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - circuit is closed`);
+        });
+
+        this.circuit.on("open", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - circuit is opened`);
+        });
+
+        this.circuit.on("halfOpen", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - circuit is halfopen`);
+        });
+
+        this.circuit.on("timeout", () => {
+
+            const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
+
+            console.log(`[${date}] - timeout occurred`);
+        });
+    }
+
 }
 
-
-const circuit = new CircuitBreaker(promise, {
-    cache: true,
-    timeout: 60000,
-    errorThresholdPercentage: 20,
-    resetTimeout: 30000,
-});
-
-circuit.fallback(() => `${baseURL} unavailable right now. Try later.`);
-
-circuit.on("fire", () => {
-
-    const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
-
-    console.log(`[${date}] - circuit has fired event`);
-});
-
-circuit.on("close", () => {
-
-    const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
-
-    console.log(`[${date}] - circuit is closed`);
-});
-
-circuit.on("open", () => {
-
-    const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
-
-    console.log(`[${date}] - circuit is opened`);
-});
-
-circuit.on("halfOpen", () => {
-
-    const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
-
-    console.log(`[${date}] - circuit is closed`);
-});
-
-circuit.on("timeout", () => {
-
-    const date: string = moment().format("DD/MM/YYYY HH:mm:ss");
-
-    console.log(`[${date}] - timeout occurred`);
-});
-
-circuit.fire().then().catch(console.error)
-
-
+new App().run();
